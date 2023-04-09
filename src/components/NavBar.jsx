9bddf6ce25b0af72703598bf38903e85a30358aa
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-04-09 21:03:02 +0530

    first

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./NavBar.scss";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <span className="text">fiverr</span>
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Fiver Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          <span>Become a Seller</span>
+          <button>Join</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
