3707aaab8545e0f6380269f2e3a0fbaab409689e
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-04-09 21:38:07 +0530

    navbar for new user

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./NavBar.scss";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const [active, setActive] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  });
+
   return (
-    <div className="navbar">
+    <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
+          {/* <Link to="/"> */}
           <span className="text">fiverr</span>
+          {/* </Link> */}
           <span className="dot">.</span>
         </div>
         <div className="links">
           <span>Fiver Business</span>
           <span>Explore</span>
           <span>English</span>
-          <span>Sign in</span>
           <span>Become a Seller</span>
+          <span>Sign in</span>
           <button>Join</button>
         </div>
       </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>test</span>
+            <span>test2</span>
+          </div>
+        </>
+      )}
     </div>
   );
 };
