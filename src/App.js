fe4d941dc36d4bc50f5aa681bebd55a0be5cf0d1
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-04-12 21:42:02 +0530

    React Router Dom and File arrangement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,71 @@
-import NavBar from "./components/NavBar";
+import NavBar from "./components/navbar/NavBar";
 import React from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Gigs from "./pages/gigs/Gigs";
+import Gig from "./pages/gig/Gig";
+import Orders from "./pages/orders/Orders";
+import MyGigs from "./pages/myGigs/MyGigs";
+import Add from "./pages/add/Add";
+import Messages from "./pages/messages/Messages";
+import Message from "./pages/message/Message";
+import Footer from "./components/footer/Footer";
 
 function App() {
+  const Layout = () => {
+    return (
+      <>
+        <NavBar />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path:"/",
+          element:<Home />
+        },
+        {
+          path:"/gigs",
+          element:<Gigs />
+        },
+        {
+          path:"/gig/:id",
+          element:<Gig />
+        },
+        {
+          path:"/orders",
+          element:<Orders />
+        },
+        {
+          path:"/mygigs",
+          element:<MyGigs />
+        },
+        {
+          path:"/add",
+          element:<Add />
+        },
+        {
+          path:"/messages",
+          element:<Messages />
+        },
+        {
+          path:"/message/:id",
+          element:<Message />
+        },
+      ]
+    },
+  ]);
+
   return (
-    <div >
-      <NavBar />
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
