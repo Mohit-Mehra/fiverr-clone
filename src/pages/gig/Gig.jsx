c5570f3bb83266d613758e0b6ab04fc7bba06108
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-04-18 22:10:43 +0530

    Gig page

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
-
+import React from "react";
+import "./Gig.scss";
+import { Slider } from "infinite-react-carousel/lib";
 const Gig = () => {
   return (
-    <div>Gig</div>
-  )
-}
+    <div className="gig">
+      <div className="container">
+        <div className="left">
+          <span className="breadCrumb">FIVERR &gt; GRAPHICS & DESIGN &gt;</span>
+          <h1>I will create AI generated art for you</h1>
 
-export default Gig
+          <div className="user">
+            <img src="" alt="" />
+            <span>Mohit Mehra</span>
+            <div className="stars">
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
+              <span>5</span>
+            </div>
+          </div>
+          <Slider slidesToShow={1} arrowsScroll={1}>
+            <img src="/img/AI.png" alt="" />
+            <img src="/img/AI2.png" alt="" />
+            <img src="/img/AI3.png" alt="" />
+            <img src="/img/AI4.png" alt="" />
+          </Slider>
+          <h2>About This Gig</h2>
+          <p>
+            I use an AI program to create images based on text prompts. This
+            means I can help you to create a vision you have through a textual
+            description of your scene without requiring any reference images.
+            Some things I&apos;ve found it often excels at are: Character
+            portraits (E.g. a picture to go with your DnD character) Landscapes
+            (E.g. wallpapers, illustrations to compliment a story) Logos (E.g.
+            Esports team, business, profile picture) You can be as vague or as
+            descriptive as you want. Being more vague will allow the AI to be
+            more creative which can sometimes result in some amazing images. You
+            can also be incredibly precise if you have a clear image of what you
+            want in mind. All of the images I create are original and will be
+            found nowhere else. If you have any questions you&apos;re more than
+            welcome to send me a message.
+          </p>
+          <div className="seller">
+            <div className="user">
+              <img src="/img/AI.png" alt="" />
+              <div className="info">
+                <span>Mohit Mehra</span>
+                <div className="stars">
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <span>5</span>
+                </div>
+                <button>Contact Me</button>
+              </div>
+            </div>
+            <div className="box">
+              <div className="items">
+                <div className="item">
+                  <span className="title">From</span>
+                  <span className="desc">USA</span>
+                </div>
+                <div className="item">
+                  <span className="title">Member since</span>
+                  <span className="desc">Aug 2022</span>
+                </div>
+                <div className="item">
+                  <span className="title">Avg. response time</span>
+                  <span className="desc">4 hours</span>
+                </div>
+                <div className="item">
+                  <span className="title">Last delivery</span>
+                  <span className="desc">1 day</span>
+                </div>
+                <div className="item">
+                  <span className="title">Languages</span>
+                  <span className="desc">English</span>
+                </div>
+              </div>
+              <hr />
+              <p>
+                My name is Anna, I enjoy creating AI generated art in my spare
+                time. I have a lot of experience using the AI program and that
+                means I know what to prompt the AI with to get a great and
+                incredibly detailed result.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Gig;
