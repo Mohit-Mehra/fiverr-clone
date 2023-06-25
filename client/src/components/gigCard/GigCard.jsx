4f9b70441686f869db1ec8ff0f80aa9a00728906
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-06-25 08:39:56 +0530

    GigCards

--- client/src/components/gigCard/GigCard.jsx
+++ client/src/components/gigCard/GigCard.jsx
@@ -23,7 +23,7 @@ const GigCard = ({ item }) => {
             "Something went wrong!"
           ) : (
             <div className="user">
-              <img src={data.img || "/img/noavatar.jpg"} alt="" />
+              <img src={data.img || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"} alt="" />
               <span>{data.username}</span>
             </div>
           )}
