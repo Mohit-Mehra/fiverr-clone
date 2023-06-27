289fd3ada58348ff90f691f5bb0bf5577eea578b
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2023-06-27 10:36:06 +0530

    Completed

--- client/src/utils/upload.js
+++ client/src/utils/upload.js
@@ -6,7 +6,17 @@ const upload = async (file) => {
   data.append("upload_preset", "fiverr");
 
   try {
-    const res = await axios.post("https://api.cloudinary.com/v1_1/mohitmehra/image/upload", data);
+    const res = await axios.post(
+      "https://res.cloudinary.com/do4gugtw3/image/upload/",
+      data,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: "335113255196343",
+          "Access-Control-Allow-Origin":"*",
+        },
+      }
+    );
 
     const { url } = res.data;
     return url;
